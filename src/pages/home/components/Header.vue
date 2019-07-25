d82a1ad0484d00e1f27a157d94b37f1e2515eeb7
HenryDong <943082205@qq.com>
2019-07-25 16:35:43 +0800

    finish header css&icon

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="header">
-    <div class="header-left">返回</div>
-    <div class="header-input">输入城市景点</div>
-    <div class="header-right">城市</div>
+    <div class="header-left"><div class="back-icon"><i class="iconfont iconfanhui"></i></div></div>
+    <div class="header-input"><i class="iconfont iconsousuo"></i>输入城市景点</div>
+    <div class="header-right">城市 <span class="arrow-icon"><i class="iconfont iconjiantou"></i></span></div>
   </div>
 </template>
 
@@ -13,19 +13,24 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+  @import "~styles/varibles"
   .header
     display: flex
     line-height: .86rem
-    background: #0bd
+    background: $bgColor
     color: #fff
     .header-left
       width: 0.64rem
       float: left
+      .back-icon
+        text-align: center
+        font-size: .4rem
     .header-input
       flex: 1
       height: .64rem
       margin-top: .12rem
       margin-left .2rem
+      padding-left: .2rem
       line-height: .64rem
       background: #fff
       border-radius: .1rem
@@ -34,4 +39,7 @@ export default {
       width: 1.24rem
       float: right
       text-align: center
+      .arrow-icon
+        margin-left: -.04rem
+        font-size: .24rem
 </style>
