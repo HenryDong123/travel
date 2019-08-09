5700dda7879f19a1f3be7b70a7b65eafee67f5ac
HenryDong <943082205@qq.com>
2019-08-09 10:04:44 +0800

    fix header

--- src/pages/detail/components/Header.vue
+++ src/pages/detail/components/Header.vue
@@ -44,12 +44,18 @@ export default {
       }
     }
   },
-  activated () {
+  mounted () {
     window.addEventListener('scroll', this.handleScroll)
   },
-  deactivated () {
+  destroyed () {
     window.removeEventListener('scroll', this.handleScroll)
   }
+  // activated () {
+  //   window.addEventListener('scroll', this.handleScroll)
+  // },
+  // deactivated () {
+  //   window.removeEventListener('scroll', this.handleScroll)
+  // }
 }
 </script>
 
